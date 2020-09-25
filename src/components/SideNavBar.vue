593b903c7692e3c47b1b1ba29c9423f81c4abb7e
Sean931231 <seanwonghh93@gmail.com>
2020-09-25 18:12:17 +0800

    update

--- src/components/SideNavBar.vue
+++ src/components/SideNavBar.vue
@@ -1,45 +1,50 @@
 <template>
-    <div id="nav">
-        <b-navbar variant="faded" type="light">
-            <b-navbar-brand tag="h1" href="#/about">Pokedex</b-navbar-brand>
+  <div id="nav">
+    <b-navbar toggleable variant="faded" type="light">
+      <b-navbar-brand tag="h1" href="#/about">Pokedex</b-navbar-brand>
 
-            <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-toggle target="navbar-toggle-collapse">
+      <template v-slot:default="{ expanded }">
+        <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+        <b-icon v-else icon="chevron-bar-down"></b-icon>
+      </template>
+      </b-navbar-toggle>
 
-            <b-navbar-nav>
-                <b-nav-item href="#">Link</b-nav-item>
-                <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-navbar-nav>
+      <b-collapse id="navbar-toggle-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Link 1</b-nav-item>
+        <b-nav-item href="#">Link 2</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+      </b-collapse>
 
-            </b-collapse>
-        </b-navbar>
-
-      <b-button variant="primary" router-link to="/">
-        Home
-      </b-button>
-       |
-      <b-button variant="primary" router-link to="/about">
-        About
-      </b-button>
-    </div>
+      <!-- <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item router-link to="/">Home</b-nav-item>
+            <b-nav-item router-link to="/about">About</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse> -->
+    </b-navbar>
+  </div>
 </template>
 
 <script>
     export default {
-        
+
     }
 </script>
 
 <style lang="scss" scoped>
-#nav {
-    padding: 30px;
+// #nav {
+//     padding: 30px;
 
-    a {
-        font-weight: bold;
-        color: #2c3e50;
+//     a {
+//         font-weight: bold;
+//         color: #2c3e50;
 
-        &.router-link-exact-active {
-        color: #bb3636;
-        }
-    }
-}
+//         &.router-link-exact-active {
+//         color: #bb3636;
+//         }
+//     }
+// }
 </style>

6bd52f72e4d738c9c2ece4b7bf6e9dc8245671c5
Sean931231 <seanwonghh93@gmail.com>
2020-10-27 14:22:10 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,15 @@
 <template>
-  <div class="home">
-    <div class="title">
-      <h1> PoKedex </h1>
-    </div>
-    <b-row align-h="center" align-v="center">
-      <b-col cols="8">
-        <div class="background-video">
-          <video-background
-            src="/video/ep1.mp4"
-            style="height: 80vh; max-height: 800px;"
-          >
-          </video-background>
+  <b-row class="home">
+    <div class="grid-container">
+      <div class="video">
+        <div class="gameboy-back">
+
         </div>
-      </b-col>
-    </b-row>
-    <b-row class="sticker">
-      <b-col class="left">
-        <b-img src="../assets/sticker/pikachu.png"></b-img>
-      </b-col>
-      <b-col class="right">
-        <b-img src="../assets/sticker/gengar.png"></b-img>
-      </b-col>
-    </b-row>
-  </div>
+      </div>
+      <div class="top">top</div>
+      <div class="bottom">bottom</div>
+    </div>
+  </b-row>
 </template>
 
 <script>
